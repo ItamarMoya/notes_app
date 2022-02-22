f127e28c2b5060b86752ac61d9e9324b47bd4dcd
itamar <itamar1501@gmail.com>
2022-02-22 14:20:23 -0600

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+
+import 'package:flutter/material.dart';
+import 'package:my_notes_app/screens/home.dart';
+
+void main(){
+  runApp(NotesApp());
+}
+
+class NotesApp extends StatelessWidget {
+  const NotesApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    );
+  }
+}
+
---
+++ lib/screens/home.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('Simple Notes App'),
+      ),
+      body: Center(
+        child:Text('body') ,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed:(){} ,
+        child: Icon(Icons.add),),
+    );
+  }
+}
